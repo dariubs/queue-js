03d89f93825c1c6a78e74c3e2ec8ddbb69f87880
Dariush Abbasi <dariushem@yahoo.com>
2015-02-08 20:18:04 +0330

    update

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+
 var Queuejs = function(){
     this._storage = [];
     this.temp = "Empty";
-}
+};
 
 Queuejs.prototype.enqueue = function(data) {
     this._storage.push(data);
-}
+};
 
 Queuejs.prototype.dequeue = function(){
     this._storage = this._storage.slice(1,this._storage.length);
-}
+};
 
 Queuejs.prototype.peek = function(){
     this.temp = this._storage[0];
     this._storage = this._storage.slice(1,this._storage.length);
     return this.temp;
-}
+};
+
+Queuejs.prototype.show = function(){
+    return this._storage[0];
+};
